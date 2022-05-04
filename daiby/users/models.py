9c0dad7e9dfb720651eddcd699262ae729b1ceea
MaksimPan <maksim.pantsialeyeu@gmail.com>
2022-05-04 19:03:48 +0300

    model user added

--- daiby/users/models.py
+++ daiby/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    nickname = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=200, null=False, blank=False)
