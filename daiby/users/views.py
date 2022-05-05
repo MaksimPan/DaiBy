74b6c0365db6b2f7d9508032c7dcb8306b34d0c5
MaksimPan <maksim.pantsialeyeu@gmail.com>
2022-05-05 17:50:55 +0300

    images added, templates updated

--- daiby/users/views.py
+++ daiby/users/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Profile
 
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    profiles = Profile.objects.all()
+
+    context = {'profiles': profiles}
+    return render(request, 'users/profiles.html', context)
 
 def profile(request, pk):
-    return render(request, 'users/profile.html')
+    profile = Profile.objects.get(id=pk)
+    context = {'profile': profile}
+    return render(request, 'users/profile.html', context)
